f052456574969066791f7e7eec8c76636837db37
georgedivery <georgedivery@email.com>
2026-01-07 15:55:57 +0200

    add footer

--- footer.php
+++ footer.php
@@ -1,10 +1,70 @@
     <footer id="colophon" class="site-footer">
-        
-    
+        <div class="shell">
+            <div class="footer-inner">
+                <a href="#" class="footer-logo">
+                    <img src="<?php echo esc_url( get_template_directory_uri())?>/assets/images/footer-logo.png" alt="#">
+                </a>
+
+                <nav class="footer-nav">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Privacy
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                Terms of Use
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                Cookie Policy
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="#">
+                                Modern Slavery Statement
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+
+                <ul class="list-socials">
+                    <li>
+                        <a href="#">
+                            <i class="fa-brands fa-youtube"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="fa-brands fa-linkedin"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            <i class="fa-brands fa-twitter"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
     </footer>
-</div><!-- #page -->
+    </div><!-- #page -->
 
-<?php wp_footer(); ?>
-</body>
-</html>
+    <?php wp_footer(); ?>
+    </body>
 
+    </html>
